84cd74c1c718208360b2af2097368c791c4daabf
amir1387ah <official.adadshenas@gmail.com>
2025-07-04 19:14:08 +0330

    Create uart_download_v1.bat

---
+++ tools/uart_download_v1.bat
@@ -0,0 +1,24 @@
+@echo off
+title=uart download
+set WORK_PATH=%~dp0
+set CURR_PATH=%cd%
+cd %WORK_PATH%
+:start
+echo,
+echo      Uart Download
+echo,
+set /p input=please input the serial port num:
+goto download
+:download
+echo COM%input%
+D:/SiFli-SDK/tools/uart_download/ImgDownUart.exe --func 0 --port COM%input% --baund 3000000 --loadram 1 --postact 1 --device SF32LB52X_NAND --file ImgBurnList.ini --log ImgBurn.log
+if %errorlevel%==0 (
+    echo Download Successful
+)else (
+    echo Download Failed
+    echo logfile:%WORK_PATH%ImgBurn.log
+)
+cd %CURR_PATH%
+
+if "%ENV_ROOT%"=="" pause
+
